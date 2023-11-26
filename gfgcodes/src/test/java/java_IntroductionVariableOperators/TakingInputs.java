69eb4f7e5008753f3a91e3efbb978629f4715b3d
Welcome <Welcome@Welcome-PC>
2023-11-26 18:29:03 +0530

    taking inputs

---
+++ gfgcodes/src/test/java/java_IntroductionVariableOperators/TakingInputs.java
@@ -0,0 +1,19 @@
+package java_IntroductionVariableOperators;
+
+public class TakingInputs {
+
+	public static void main(String[] args) {
+		int a = 5;
+		float b=(float) 5.256;
+		long c = 568746854;
+		byte d = 120;
+		String s = "geeks for geeks";
+		
+		 System.out.println(a);
+		 System.out.println(b);
+		 System.out.println(c);
+		 System.out.println(d);
+		 System.out.println(s);
+	}
+
+}
---
+++ gfgcodes/src/test/java/java_IntroductionVariableOperators/TakingInputsFromUser.java
@@ -0,0 +1,31 @@
+package java_IntroductionVariableOperators;
+
+import java.util.Scanner;
+
+public class TakingInputsFromUser {
+
+	public static void main(String[] args) {
+
+		        Scanner sc = new Scanner(System.in);
+		        System.out.println("Enter the value of a");		        
+				int a = sc.nextInt();
+				System.out.println("Enter the value of b");	
+				float b = sc.nextFloat();
+				System.out.println("Enter the value of c");	
+				double c = sc.nextDouble();
+				System.out.println("Enter the value of d");	
+				byte d = sc.nextByte();
+				System.out.println("Enter the value of s");	
+				String s = sc.next();
+				
+				 System.out.println(a);
+				 System.out.println(b);
+				 System.out.println(c);
+				 System.out.println(d);
+				 System.out.println(s);
+
+				 
+				
+		    }
+		    
+		}
